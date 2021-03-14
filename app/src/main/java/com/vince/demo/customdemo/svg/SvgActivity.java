115c43d2b408949ba98ff4c408763c7425bd507a
vince <toumubaoqiong@foxmail.com>
2021-03-14 12:00:14 +0800

    新增svg demo

---
+++ app/src/main/java/com/vince/demo/customdemo/svg/SvgActivity.java
@@ -0,0 +1,15 @@
+package com.vince.demo.customdemo.svg;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vince.demo.customdemo.R;
+
+public class SvgActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_svg);
+    }
+}
